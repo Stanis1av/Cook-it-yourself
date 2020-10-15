5121d936d9ad09348c6bd4176a785b943e194c1d
Stanis1av <krasavin.stanislaus@gmail.com>
2020-10-15 16:24:21 +0400

    Editing migrate

--- db/migrate/20201015121621_cook_blogs.rb
+++ db/migrate/20201015121621_cook_blogs.rb
@@ -1,4 +1,11 @@
 class CookBlogs < ActiveRecord::Migration[6.0]
   def change
+    create_tables :posts do |t|
+      t.text :autor
+      t.text :content
+      t.string :path_to_image
+
+      t.timestamps
+    end
   end
 end
